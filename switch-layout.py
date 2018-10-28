7fe196c501aa3fd5d9a24c539f113dbc7a7cc782
Kirill K <kovalev.kirill.a@gmail.com>
2018-10-28 20:33:33 +0300

    Revert "Add CapsLock code as example"
    This reverts commit 72fea436a973fcb95dd028fdc11b6d5fef574b73.

--- switch-layout.py
+++ switch-layout.py
@@ -13,7 +13,6 @@ SWITCH_SHORTCUTS = [
     # Examples
     # {keyboard.Key.shift, keyboard.Key.ctrl},  # Shift + Ctrl
     # {keyboard.Key.cmd, keyboard.Key.space},  # Super + Space
-    # {keyboard.KeyCode(65509)},  # CapsLock
 ]
 
 # How many layouts do you have?
